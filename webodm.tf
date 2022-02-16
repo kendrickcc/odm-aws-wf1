e1149320b79c447f81ac4abc166e3592f2e4d164
Chris Kendrick <kendrickcc@gmail.com>
2022-02-16 13:16:28 -0600

    error with private_ip

--- webodm.tf
+++ webodm.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "webodm" {
   instance_type               = var.instance_type
   key_name                    = var.pub_key
   subnet_id                   = aws_subnet.odm_public_subnet.id
-  private_ips                 = var.private_ip_webodm
+  private_ip                  = var.private_ip_webodm
   vpc_security_group_ids      = [aws_security_group.odm.id]
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
