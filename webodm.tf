8ec8c11898a6a434e4ca90a1f16b81aa04b52c56
Chris Kendrick <kendrickcc@gmail.com>
2022-03-09 19:13:38 -0600

    variable fix

--- webodm.tf
+++ webodm.tf
@@ -125,7 +125,7 @@ data "template_file" "webodm" {
   template = file("webodm.tpl")
   vars = {
     ssh_key = var.pub_key_data
-    pem_key = var.pri_key_data
+    pem_key = var.pem_key_data
   }
 }
 data "template_file" "nodeodm" {
